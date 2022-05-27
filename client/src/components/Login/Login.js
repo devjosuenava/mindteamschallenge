b5899861913770a413c5a45ce5a18c2ef34c854e
Josue Nava <devjosuenava@gmail.com>
2022-05-26 18:02:13 -0600

    first commit

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,55 @@
+import './Login.css';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+async function loginUser(credentials){
+  return fetch('http://localhost:8080/api/auth/signin', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(async data => {      
+      const result = await data.json();      
+      return result;
+    });
+}
+
+const Login = ({ setToken }) => {
+  const [ email, setEmail ] = useState();
+  const [ password, setPassword ] = useState();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({ email, password });
+    console.log(token)
+    setToken(token);
+  }
+
+
+  return (
+    <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form onSubmit={ handleSubmit }>
+        <label>
+          <p>Username</p>
+          <input type="text" onChange={ e => setEmail(e.target.value) }/>
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" onChange={ e => setPassword(e.target.value) }/>
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
+}
+
+export default Login
---
+++ server/app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "mindteams-secret-key"
+  };
